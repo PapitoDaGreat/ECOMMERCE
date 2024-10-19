e7ed7d3561c3b0bcc7b8dcf3422d6856fe23e284
Adrian Louie Tirado <adrianlouie.tirado@sdca.edu.ph>
2024-10-19 13:32:40 +0800

    hehehe

--- registration.php
+++ registration.php
@@ -1,5 +1,6 @@
-<?php
-  require_once("includes\\header.php");
+
+<?php 
+    require_once("includes\header.php");
 ?>
 
     <!-- Navbar -->
@@ -14,31 +15,28 @@
                         <h4>Create Your Account</h4>
                     </div>
                     <div class="card-body">
+                        <!-- message response -->
+                        <?php if(isset($_GET["success"])){ ?>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong><?php echo $_GET["success"]; ?></strong> 
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php } ?>    
 
-                    <!-- message response -->
-                     <?php if(isset($_GET["success"])){?>
-
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-              <strong><?php echo $_GET["success"]; ?></strong> 
-             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
-        <?php } ?>
-
-        <?php if(isset($_GET["error"])){?>
-              <div class="alert alert-danger alert-dismissible fade show" role="alert">
-              <strong><?php echo $_GET["error"]; ?></strong> 
-             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
-              <?php } ?>
-
+                        <?php if(isset($_GET["error"])){ ?>
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong><?php echo $_GET["error"]; ?></strong> 
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php } ?>    
                         <form action="authRegister.php" method="POST">
                             <div class="mb-3">
                                 <label for="fullName" class="form-label">Full Name</label>
                                 <input type="text" class="form-control" id="fullName" name="fullName" placeholder="Enter your full name" required>
                             </div>
                             <div class="mb-3">
-                                <label for="username" class="form-label">Username</label>
-                                <input type="username" class="form-control" id="username" name="username" placeholder="Enter your username" required>
+                                <label for="username" class="form-label">Email Address</label>
+                                <input type="text" class="form-control" id="username" name="username" placeholder="Enter your email" required>
                             </div>
                             <div class="mb-3">
                                 <label for="password" class="form-label">Password</label>
